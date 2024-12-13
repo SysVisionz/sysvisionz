5d761a705cc4a1d5164c15aa49cc0d33bbbad693
Colin Brennan <colin@sysvisionz.com>
2024-12-13 10:23:05 -0800

    Dude stop with the loading thing

--- src/app/proposal/page.tsx
+++ src/app/proposal/page.tsx
@@ -7,7 +7,7 @@ const Proposal: FC = () => {
 	return (
 		<div>
 			<h1>Proposal</h1>
-			<Loading message="Loading proposal..." time={2500} onIsLoaded={() => console.log('loaded!')} at={{10: () => console.log('10%'), 50: () => console.log("50%")}} />
+			<Loading message="Loading proposal..." delay={2500} onIsLoaded={() => console.log('loaded!')} at={{10: () => console.log('10%'), 50: () => console.log("50%")}} />
 		</div>
 	)
 }
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,86 +1,70 @@
 'use client'
-import { FC, useEffect, useRef, useState, ReactElement } from "react";
+import { FC, useEffect, useRef, useState, ReactElement, ReactNode, LegacyRef } from "react";
+import style from './scss/Loading.module.scss'
 
-function Loading ({message, percent, at, onIsLoaded}: {message?: string, percent?: number, at?: {[percentage: number]: () => void}, onIsLoaded?: () => void}): ReactElement
-function Loading ({message, at, time, onIsLoaded}: {message?: string, at?: {[percentage: number]: () => void}, onIsLoaded?: () => void, 
-	/** time must be positive */
-	time?: number}): ReactElement
-function Loading ({onIsLoaded, message, percent, at = {}, time}: {message?: string, percent?: number, at?: {[percentage: number]: () => void}, time?: number, onIsLoaded?: () => void}): ReactElement {
-	const [curr, setCurre] = useState<number>(percent ?? 0)
-	const setCurr = (v: number) => {
-		if (was.current){
-			was.current[0] =  v;
-		}
-		setCurre(v)
-	}
-	const was = useRef<[number, number]>([curr, curr])
-	const [prog, setProg] = useState<number>(curr)
-	const timer = useRef<NodeJS.Timeout>()
-	const timers = useRef<SetIterator<(() => NodeJS.Timeout)>>()
-	const testingTimer = useRef<[NodeJS.Timeout, number]>()
+function Loading ({message, percent, at, onIsLoaded, className}: {message?: string, percent?: number, at?: {[percentage: number]: () => void}, className?: string | {
+	/** class name for the containing element */
+	container?: string,
+	/** class name for the full bar element */
+	bar?: string,
+	/** class name for the progress bar element */
+	progress?: string
+}, onIsLoaded?: () => void}): ReactElement
+function Loading ({message, at, delay, onIsLoaded, className}: {message?: string, at?: {[percentage: number]: () => void}, className?: string | {
+	/** class name for the containing element */
+	container?: string,
+	/** class name for the full bar element */
+	bar?: string,
+	/** class name for the progress bar element */
+	progress?: string
+}, onIsLoaded?: () => void, 
+	/** delay must be positive */
+	delay?: number}): ReactElement
+function Loading ({message, className, at, ...args }: {message?: string, percent?: number, at?: {[percentage: number]: () => void}, delay?: number, className?: string | {
+	/** class name for the containing element */
+	container?: string,
+	/** class name for the full bar element */
+	bar?: string,
+	/** class name for the progress bar element */
+	progress?: string
+}, onIsLoaded?: () => void}): ReactElement {
+	const was = useRef<`${number}%`>(args.delay ? '0%' : `${args.percent as number}%`)
+	const container = useRef<HTMLDivElement>(null)
+	const {container: cName, container: contain, progress} = typeof className === 'object' ? className : {container: className}
+	const percent = useLoadStyle(args.delay || `${args.percent}%` as `${number}%`)
+	const timers = useRef<NodeJS.Timeout[]>([])
 	useEffect(() => {
-		testingTimer.current = [setInterval(() => {
-			if (testingTimer.current){
-				testingTimer.current[1] += 50;
-			}
-		}, 50), 0]
-		if (time){
-			if ( time < 50 ){
-				setProg(100)
-			}
-			else if (time < 500){
-				setCurr(time);
-			}
-			else {
-				const theTimers = new Set<(() => NodeJS.Timeout)>()
-				let total = 0;	
-				for (let i = 0; i < 5 && total < time - 500; i++){
-					const t = 50 + Math.floor(time/3 * Math.random())
-					total += t
-					theTimers.add(() => setTimeout(() => {
-						const v = curr + t;
-						setCurr( v > time ? time : v)
-						timers.current?.next().value?.()
-					}, t))
-				}
-				theTimers.add(() => setTimeout(() => {
-					setCurr(time)
-				}, time - total))
-				timers.current = theTimers.values()
-				let i = timers.current.next().value
-				i?.()
+		for (const i in at){
+			if (i <= percent && i > was.current){
+				timers.current.push(setTimeout(()=> {
+					at[Number(i)]()
+				}, 160))
 			}
 		}
-	}, [])
-	useEffect(() => {
-		if (prog < curr){
-			const newProg = prog + (curr - prog)/1.35
-			for (const i in at){
-				if (Number(i) > was.current[1] && Number(i) <= newProg){
-					at[i]()
-				}
-			}
-			if (was.current[1] !== time && newProg === time){
-				console.log(testingTimer.current?.[1])
-				onIsLoaded?.()
-			}
-			was.current[1] = newProg
-			console.log(was.current)
-			clearTimeout(timer.current)
-			timer.current = setTimeout(() => setProg( curr - newProg < (curr - was.current[0])/20 ? curr : newProg), 50)
-		}
-	}, [curr, prog])
-	useEffect(() => {
-		if (percent){
-			setCurr(percent)
-		}
+		was.current = percent
 	}, [percent])
 	return (
-		<div>
-			{message && <p>{message}</p>}
-			<progress value={prog} max={time && time > 50 ? time : 100} />
+	<div className={cName}>
+		{message && <p>{message}</p>}
+		<div ref={container} className={style.container} >
+			<div className={style.bar} style={{width: percent}}></div>
 		</div>
+	</div>
 	)
+}
+
+const useLoadStyle = (val: number | `${number}%`): `${number}%` => {
+	const [percent, setPercent] = useState<`${number}%`>(typeof val === 'string' ? val : `0%`)
+	const timers = useRef< (() => NodeJS.Timeout)[] >(Array(5))
+	useEffect(() => {
+		if (typeof val === 'string') {
+			setPercent(val)
+		}
+		else {
+		// TODO: finish this later, dude. There's no reason you need to do this right now, it's so far from MVP	
+		}
+	}, [val])
+	return percent;
 }
 
 export default Loading

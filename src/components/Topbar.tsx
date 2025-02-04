76b081f555601a129a42c059d7815d162d636815
Colin Brennan <colin@sysvisionz.com>
2025-02-04 14:05:45 -0800

    styling fixes

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -4,12 +4,20 @@ import style from './scss/Topbar.module.scss'
 import { FC, useContext, useEffect, useRef, useState } from "react";
 import { userContext } from '../contexts/user';
 import Link from 'next/link';
+import { useDelay } from '~/shared/utils';
 
 /** this component changes height, as well as the logo size/type */
 const Topbar: FC = () => {
 	const [atTop, setAtTop] = useState<boolean>(true)
+	const [tablet, setTablet] = useState<boolean>(false)
 	const spacer = useRef<HTMLDivElement>(null)
 	const iObserv = useRef<IntersectionObserver>()
+	const setTabSize = useDelay({onEnd: () => {
+		if (tablet !== window.innerWidth <= 768){
+			setTablet(window.innerWidth <= 768)
+		}
+	}}, 300)
+	const observer = useRef<ResizeObserver>()
 	useEffect(() => {
 		if (!iObserv.current){
 			iObserv.current = new IntersectionObserver((entries) => {
@@ -20,26 +28,31 @@ const Topbar: FC = () => {
 				}
 			}, {threshold: 0.8})
 		}
-		if (!(typeof window === 'undefined')){
+		if (!observer.current) { observer.current = new ResizeObserver(setTabSize) }
+		if (typeof window !== 'undefined'){
 			setAtTop(window.scrollY <= 70)
+			if (tablet !== window.innerWidth <= 768){
+				setTablet(window.innerWidth <= 768)
+			}
 		}
-		if (spacer.current){iObserv.current.observe(spacer.current)}
+		if (spacer.current){ iObserv.current.observe(spacer.current) }
+		if (observer.current && typeof window !== 'undefined') { observer.current.observe(document.body) }
 		return () => {
 			if (spacer.current) {iObserv.current?.unobserve(spacer.current)}
+			if (observer.current) {observer.current?.disconnect()}
 		}
-		
 	}, [])
 	const {user} = useContext(userContext)
 	return<>
 		<div ref={spacer} className={`${style.spacer}${atTop ? ` ${style.top}` : ""}`}></div>
 		<div className={`${style.topbar}${atTop ? ` ${style.top}` : ''}`}>
-			<div className={style.logo}><Link href="/"><Logo {...(atTop ? {} : {
+			<div className={style.logo}><Link href="/"><Logo {...(atTop ? tablet ? {style: 'small'} : {} : {
 				color: 'blue',
 				style: 'small'
 			})} /></Link></div>
 			<div className={style.links}>
-				<Link href="/">Home</Link>
-				<Link href="/services">Services</Link>
+				<Link href="/"><h2>Home</h2></Link>
+				<Link href="/services"><h2>Services</h2></Link>
 			</div>
 			<div style={{display: 'none'}}>
 				{user.privLevel ? <div>{user.displayName}</div> : <button>Login</button>}

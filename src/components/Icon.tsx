e40cf584e5d960e913d253a29f58a91444da9e65
Colin Brennan <colin@sysvisionz.com>
2025-02-18 18:49:22 -0800

    Fixes to styling

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,13 +1,18 @@
 import icons, {Icons} from "~/icons"
 import Image from "next/image"
-import stylef from './scss/Icon.module.scss'
+import style from './scss/Icon.module.scss'
+import { ReactNode } from "react"
 
-const Icon = <T extends keyof Icons, I extends keyof Icons[T]>({category, icon, size, ...props}: {category: T, icon: I, size?: 'large' | 'small' |'medium'| 'tiny', height?: number, width?: number, alt?: string}) => {
+function Icon<T extends keyof Icons, I extends keyof Icons[T]>({category, icon, ...props}: {category: T, icon: I, size: 'large' | 'small' |'medium'| 'tiny', alt?: string}): ReactNode
+function Icon<T extends keyof Icons, I extends keyof Icons[T]>({category, icon, ...props}: {category: T, icon: I, fill: true, alt?: string}): ReactNode
+function Icon<T extends keyof Icons, I extends keyof Icons[T]>({category, icon, ...props}: {category: T, icon: I, height: number, width: number, alt?: string}): ReactNode
+function Icon<T extends keyof Icons, I extends keyof Icons[T]>({category, icon, ...props}: {category: T, icon: I}): ReactNode
+function Icon<T extends keyof Icons, I extends keyof Icons[T]>({category, icon, ...props}: {category: T, icon: I, size?: 'large' | 'small' |'medium'| 'tiny', fill?: boolean, height?: number, width?: number, alt?: string}) {
 	const theIcon = icons[category][icon] as SVGItem
-	const {alt = `${category} ${icon as string}`} = props
-	const height = props.height || {large: 48, small: 32, medium: 40, tiny: 24}[size || 'medium'] || 40
-	const width = props.width || {large: 48, small: 32, medium: 40, tiny: 24}[size || 'medium'] || 40
-	return <Image className={stylef.icon} src={theIcon.src} height={height} width={width} alt={alt} />
+	const {height, width, size, fill, alt = `${category} ${icon as string}`} = props
+	return <div className={style.icon}><Image className={style.icon} src={theIcon.src} {...{
+		...(height ? {height, width}: size ? {height: {large: 48, small: 32, medium: 40, tiny: 24}[size], width: {large: 48, small: 32, medium: 40, tiny: 24}[size]} : fill ? {fill} : {fill: true})
+	 }} alt={alt} /></div>
 }
 
 export default Icon
--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -2,9 +2,9 @@ import { MouseEventHandler } from "react"
 import style from './scss/IconButton.module.scss'
 import Icon from "~/Icon"
 import { Icons } from "~/icons"
-
-const IconButton = <T extends keyof Icons, I extends keyof Icons[T]>({onClick, navigate, category, icon, height, width, alt}: {navigate?: string, onClick?: MouseEventHandler<HTMLButtonElement>, category: T, icon: I, height?: number, width?: number, alt?: string}) => {
-	const button = <button className={style.button} onClick={(evt) => onClick?.(evt)}><Icon category={category} icon={icon} height={height} width={width} alt={alt}/></button>
+ 
+const IconButton = <T extends keyof Icons, I extends keyof Icons[T]>({onClick, navigate, category, icon, ...props}: {navigate?: string, onClick?: MouseEventHandler<HTMLButtonElement>, category: T, icon: I} & Omit<Parameters<typeof Icon>[0], 'icon' | 'category'>) => {
+	const button = <button className={style.button} onClick={(evt) => onClick?.(evt)}><Icon category={category} icon={icon} {...props}/></button>
 	return navigate ? <a href={`${navigate.match(/\.(com|net|org|io)(\/*|)$/) ? 'https://' : ''}${navigate}`}>{button}</a> : button
 }
 

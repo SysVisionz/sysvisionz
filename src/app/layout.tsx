0e28a5bdc54166695f833e7373c1945f2c137e15
Colin Brennan <colin@sysvisionz.com>
2025-03-17 12:34:55 -0700

    Made it so fonts can be rotated on dev.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,45 @@
+'use client'
 import type { Metadata } from "next";
 import style from "./Style.module.scss";
 import SiteProvider from "../contexts/site";
 import Footer from "~/Footer";
 import Parallax from "~/Parallax";
 import { dev } from "~/images";
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import Topbar from "~/Topbar";
 import "./global.css"
 import favicon from "./favicon.ico";
+import Button from "~/Button";
 
 
-export const metadata: Metadata = {
-  title: "SysVisionz",
-  description: "SysVisionz, a website for the future",
-}
+// export const metadata: Metadata = {
+//   title: "SysVisionz",
+//   description: "SysVisionz, a website for the future",
+// }
 
 export default function RootLayout({
   children
 }: Readonly<{
   children: ReactElement<HTMLDivElement>[];
 }>) {
-
+  const [header, setHeader] = useState<number>(5);
+  const [body, setBody] = useState<number>(0);
+  useEffect(() => {
+    if (body > 6) {
+      setBody(0);
+    }
+    if (header > 6) {
+      setHeader(0);
+    }
+  }, [body, header]);
   return (
     <html lang="en">
       <link rel="apple-touch-icon" href={favicon.src} />
-      <body className={style.body}>
+      <body className={style.body + " " + style[`body-header-${header}`] + " " + style[`body-body-${body}`]}>
         <Parallax image={dev}>
           <SiteProvider>
+              {typeof window !== 'undefined' && location.host === "localhost:8082" ? <div className={style.changers}><Button onClick={() => {setHeader(header + 1)}}>Headers</Button>
+              <Button onClick={() => {setBody(body + 1)}}>Body</Button></div> : null}
               <Topbar/>
                 <main className={style.main} id="main">
                   <div className={style.layout}>
